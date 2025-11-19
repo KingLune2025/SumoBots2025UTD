4c9869e307ffe1b00de1ac605c56d0ecebb27478
KingLune2025 <arpeet.nandi@gmail.com>
2025-11-19 17:58:00 -0600

    fixed

--- include/BoardConfig.h
+++ include/BoardConfig.h
@@ -20,13 +20,14 @@ constexpr float MAX_ADC = 4095.0;
 #elif defined(BOARD_UNO)
 // --- Arduino UNO R3 Pin Mapping ---
 constexpr int PIN_IR_DISTANCE_SENSOR_RIGHT = A0;
-constexpr int PIN_IR_DISTANCE_SENSOR_MIDDLE = A1;
-constexpr int PIN_IR_DISTANCE_SENSOR_LEFT = A2;
+constexpr int PIN_IR_DISTANCE_SENSOR_MIDDLELEFT = A1;
+constexpr int PIN_IR_DISTANCE_SENSOR_MIDDLERIGHT = A2;
+constexpr int PIN_IR_DISTANCE_SENSOR_LEFT = A3;
 
-constexpr int PIN_BORDER_SENSOR_R1 = A4;
-constexpr int PIN_BORDER_SENSOR_R2 = A5;
-constexpr int PIN_BORDER_SENSOR_L1 = A6;
-constexpr int PIN_BORDER_SENSOR_L2 = A7;
+
+constexpr int PIN_BORDER_SENSOR_RIGHT = A4;
+constexpr int PIN_BORDER_SENSOR_LEFT = A5;
+constexpr int PIN_BORDER_SENSOR_BACK = A6;
 
 constexpr float VREF = 5.0;
 constexpr float MAX_ADC = 1023.0;
